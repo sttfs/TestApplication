e7f05f3fb329806c4adfd76e4250a01cbd125711
sttfs <>
2017-07-20 12:24:22 +0800

    change a little

--- app/src/main/java/com/test/acl/MainActivity.java
+++ app/src/main/java/com/test/acl/MainActivity.java
@@ -1,7 +1,10 @@
 package com.test.acl;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/test/acl/TestActivity.java
@@ -0,0 +1,19 @@
+package com.test.acl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by acl on 2017/7/20.
+ */
+
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(this.getClass().getName());
+    }
+}
